59ab6534415a0feda043af2ec7a36dab26bec8f6
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2022-07-23 00:17:53 -0400

    fix: add inline plugin to create a single html file
    this allows to create fully working offline apps and be read by browser without worrying too much about permissions and CORS policies
    
    some handlings are different, for instance, assets needs to be in the public folder or they won't load, despite changing the base to a relative file

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import svelteLogo from './assets/svelte.svg';
 	import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
 	<div>
 		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite Logo" />
+			<img src="./vite.svg" class="logo" alt="Vite Logo" />
 		</a>
 		<a href="https://svelte.dev" target="_blank">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+			<img src="./svelte.svg" class="logo svelte" alt="Svelte Logo" />
 		</a>
 	</div>
 	<h1>Vite + Svelte</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '',
+
 	plugins: [
 		svelte({
 			experimental: { generateMissingPreprocessorSourcemaps: true },
 		}),
+		viteSingleFile(),
 	],
 });
