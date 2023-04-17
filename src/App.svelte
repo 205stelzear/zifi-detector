ef71dbdee24f2a33b0e95c15423134fc9cf50a03
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2023-04-17 11:28:28 -0400

    refactor: move individual ring arrays to single object array
    this makes it much easier to work with a single object and prevents missing values in non-linked arrays

--- src/App.svelte
+++ src/App.svelte
@@ -6,9 +6,29 @@
 
 	let curr = 4;
 
-	const ringScaleArr = [1, 1.1, 1.2, 1.3, 1.3, 1.3, 1.5, 1.5, 1.7, 1.8, 1.8, 2.0, 2.0, 2.2, 2.2];
-	const ringSpeedArr = [4000, 3800, 3400, 3300, 3200, 3100, 3000, 2500, 2500, 2300, 2200, 2100, 2000, 1500, 500];
-	const ringThicknessArr = [35, 40, 40, 45, 50, 50, 50, 55, 55, 60, 60, 65, 70, 75, 90];
+	type Ring = {
+		scale: number;
+		speed: number;
+		thickness: number;
+	};
+
+	const rings: Ring[] = [
+		{ scale: 1, speed: 4000, thickness: 35 },
+		{ scale: 1.1, speed: 3800, thickness: 40 },
+		{ scale: 1.2, speed: 3400, thickness: 40 },
+		{ scale: 1.3, speed: 3300, thickness: 45 },
+		{ scale: 1.3, speed: 3200, thickness: 50 },
+		{ scale: 1.3, speed: 3100, thickness: 50 },
+		{ scale: 1.5, speed: 3000, thickness: 50 },
+		{ scale: 1.5, speed: 2500, thickness: 55 },
+		{ scale: 1.7, speed: 2500, thickness: 55 },
+		{ scale: 1.8, speed: 2300, thickness: 60 },
+		{ scale: 1.8, speed: 2200, thickness: 60 },
+		{ scale: 2.0, speed: 2100, thickness: 65 },
+		{ scale: 2.0, speed: 2000, thickness: 70 },
+		{ scale: 2.2, speed: 1500, thickness: 75 },
+		{ scale: 2.2, speed: 500, thickness: 90 },
+	];
 
 	let secondsBeforeNotMoreIndicator = 20;
 	// let secondsBeforeNotMoreIndicator = 5;
@@ -18,7 +38,7 @@
 	async function handleTimerEnd() {
 		isPulsing = true;
 
-		if (curr == ringScaleArr.length - 1) {
+		if (curr == rings.length - 1) {
 			if (finishInterval) {
 				return;
 			}
@@ -64,7 +84,7 @@
 			<div
 				class="wifi-logo"
 				class:pulsing={isPulsing}
-				style="--ring-scale: {ringScaleArr[curr]}; --ring-speed: {ringSpeedArr[curr]}ms; --ring-thickness: {ringThicknessArr[curr]}%;"
+				style="--ring-scale: {rings[curr]?.scale}; --ring-speed: {rings[curr]?.speed}ms; --ring-thickness: {rings[curr]?.thickness}%;"
 			/>
 		</div>
 
@@ -79,4 +99,4 @@
 	<div class="right" on:click={(e) => handleRightControl()} />
 </div>
 
-<Indicator {curr} indicatorSize={ringScaleArr.length} />
+<Indicator {curr} indicatorSize={rings.length} />
